b507aefe28fbcc31f0abe2983a109e4d134f6dbd
Anton <antengman@gmail.com>
2020-08-21 18:53:43 +0200

    Open file resources/Add.adb and read line-by-line and print each line

--- src/main.adb
+++ src/main.adb
@@ -5,7 +5,5 @@ with read_file;
 procedure Main is
 file_value : Integer;
 begin
-   Put_Line("Hello World!");
-   file_value := read_file(6);
-   Put_Line("Value of file: " & Integer'Image(file_value));
+   file_value := read_file(0);
 end Main;
--- src/read_file.adb
+++ src/read_file.adb
@@ -1,4 +1,15 @@
+with Ada.Text_IO; use Ada.Text_IO;
+
 function read_file(I : Integer) return Integer is
+   Input : File_Type;
 begin
-   return 17 + I;
+   Open(File => Input,
+        Mode => In_File,
+        Name => "resources/Add.asm");
+
+   while not End_Of_File(Input) loop
+      Put_Line(Get_Line(Input));
+   end loop;
+   Close(Input);
+   return 1;
 end read_file;
