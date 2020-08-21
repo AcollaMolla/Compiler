cefb0fbda6ad18f07ee60503cf969444ab92ce8f
Anton <antengman@gmail.com>
2020-08-21 18:40:17 +0200

    Add read_file.adb, for now it will only return an integer

--- src/main.adb
+++ src/main.adb
@@ -1,8 +1,11 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+with read_file;
 
 procedure Main is
-
+file_value : Integer;
 begin
    Put_Line("Hello World!");
+   file_value := read_file(6);
+   Put_Line("Value of file: " & Integer'Image(file_value));
 end Main;
---
+++ src/read_file.adb
@@ -0,0 +1,4 @@
+function read_file(I : Integer) return Integer is
+begin
+   return 17 + I;
+end read_file;
